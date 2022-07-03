11106f032c6111ed45a0fcad0f8dff28fb932414
inkobako <hargreaves.bryant@gmail.com>
2022-07-02 21:39:11 -0700

    Initial Commit

---
+++ storyboard/storyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  storyboardApp.swift
+//  storyboard
+//
+//  Created by Bryant Hargreaves on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct storyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
